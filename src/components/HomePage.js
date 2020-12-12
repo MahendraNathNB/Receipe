28cd81c9ca2db757ca886b17c29a936413f5c3a4
MahendraNathNB <62131156+MahendraNathNB@users.noreply.github.com>
2020-12-13 01:03:56 +0530

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,6 +5,7 @@ import RecipeCard from './recipe/RecipeCard'
 function HomePage(props){
     return(
             <div className="container">
+        <h1> Home Page </h1>
                 { props.recipes ?
                     <div className="card-deck m-3">
                         { props.recipes.map( (recipe,i) => {
